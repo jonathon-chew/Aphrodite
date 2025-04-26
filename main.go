6a104d69ec82f9e2b8a3c9b3b2f50558bfba9497
Jonathon Chew <>
2025-04-27 00:01:25 +0100

    Added an option package to main.go

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package aphrodite
 import "fmt"
 
 // Colour prints the message in the specified color.
-func Colour(option, message string) {
+func Colour(option, color, message string) {
     colour := map[string]string{
         "Black":  "\x1b[0;30m",
         "Red":    "\x1b[0;31m",
@@ -14,8 +14,10 @@ func Colour(option, message string) {
         "Cyan":   "\x1b[0;36m",
         "White":  "\x1b[0;37m",
     }
-    reset := "\x1b[0m"
-    colourOption := colour[option]
-    fmt.Printf("%s%s%s\n", colourOption, message, reset)
+		if option == "Color" || option == "color"{
+			reset := "\x1b[0m"
+			colourOption := colour[color]
+			fmt.Printf("%s%s%s\n", colourOption, message, reset)
+	}
 }
 
