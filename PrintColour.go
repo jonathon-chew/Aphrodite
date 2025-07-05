d2e0a26df87e0b6b628a0fda5fbe2ac4de923f84
Jonathon Chew <>
2025-07-05 19:51:05 +0100

    feat: converted PrintColour to match ReturnColour, moved generic functions and maps to the main file

--- PrintColour.go
+++ PrintColour.go
@@ -5,137 +5,90 @@ import (
 	"strings"
 )
 
-// (#1) TODO: Also refactor like ReturnColour to remove the option argument from the function call and split into it's own functions
+/*
+Options: Black, Red, Green, Yellow, Blue, Purple, Cyan, White
+Can error if colour not found
+*/
+func PrintColour(color, message string) {
 
-// (#2) TODO: Assess performance - would it be better to just load the map relevent on each individual function rather than in one big function / global
-
-// Options: Black, Red, Green, Yellow, Blue, Purple, Cyan, White
-func PrintColour(option, color, message string) {
-
-	colour := map[string]string{
-		"Black":  "\x1b[0;30m",
-		"Red":    "\x1b[0;31m",
-		"Green":  "\x1b[0;32m",
-		"Yellow": "\x1b[0;33m",
-		"Blue":   "\x1b[0;34m",
-		"Purple": "\x1b[0;35m",
-		"Cyan":   "\x1b[0;36m",
-		"White":  "\x1b[0;37m",
-	}
-
-	Bold := map[string]string{
-		"Black":  "\x1b[1;30m",
-		"Red":    "\x1b[1;31m",
-		"Green":  "\x1b[1;32m",
-		"Yellow": "\x1b[1;33m",
-		"Blue":   "\x1b[1;34m",
-		"Purple": "\x1b[1;35m",
-		"Cyan":   "\x1b[1;36m",
-		"White":  "\x1b[1;37m",
-	}
-
-	Underline := map[string]string{
-		"Black":  "\x1b[4;30m",
-		"Red":    "\x1b[4;31m",
-		"Green":  "\x1b[4;32m",
-		"Yellow": "\x1b[4;33m",
-		"Blue":   "\x1b[4;34m",
-		"Purple": "\x1b[4;35m",
-		"Cyan":   "\x1b[4;36m",
-		"White":  "\x1b[4;37m",
-	}
-
-	Background := map[string]string{
-		"Black":  "\x1b[40m",
-		"Red":    "\x1b[41m",
-		"Green":  "\x1b[42m",
-		"Yellow": "\x1b[43m",
-		"Blue":   "\x1b[44m",
-		"Purple": "\x1b[45m",
-		"Cyan":   "\x1b[46m",
-		"White":  "\x1b[47m",
-	}
-
-	High_Intensity := map[string]string{
-		"Black":  "\x1b[0;90m",
-		"Red":    "\x1b[0;91m",
-		"Green":  "\x1b[0;92m",
-		"Yellow": "\x1b[0;93m",
-		"Blue":   "\x1b[0;94m",
-		"Purple": "\x1b[0;95m",
-		"Cyan":   "\x1b[0;96m",
-		"White":  "\x1b[0;97m",
-	}
-
-	Bold_High_Intensity := map[string]string{
-		"Black":  "\x1b[1;90m",
-		"Red":    "\x1b[1;91m",
-		"Green":  "\x1b[1;92m",
-		"Yellow": "\x1b[1;93m",
-		"Blue":   "\x1b[1;94m",
-		"Purple": "\x1b[1;95m",
-		"Cyan":   "\x1b[1;96m",
-		"White":  "\x1b[1;97m",
-	}
-
-	High_Intensity_backgrounds := map[string]string{
-		"Black":  "\x1b[0;100m",
-		"Red":    "\x1b[0;101m",
-		"Green":  "\x1b[0;102m",
-		"Yellow": "\x1b[0;103m",
-		"Blue":   "\x1b[0;104m",
-		"Purple": "\x1b[0;105m",
-		"Cyan":   "\x1b[0;106m",
-		"White":  "\x1b[0;107m",
-	}
-
-	reset := "\x1b[0m"
-
-	var optionChoice string = strings.Replace(option, " ", "_", -1)
 	var colourChoice string = strings.ToUpper(string(color[0])) + color[1:]
 
-	var command string = strings.ToLower(optionChoice)
-
-	if command == "color" || command == "colour" || command == "text" || command == "rune" {
-		if colourChoice == "Rainbow" {
-			messageLength := len(message)
-			for i := 0; i < messageLength; i++ {
-				r, g, b := rainbow(i)
-				fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m%s", r, g, b, message[i], reset)
-			}
-		} else {
-			colourOfOptionPicked := colour[colourChoice]
-			fmt.Printf("%s%s%s", colourOfOptionPicked, message, reset)
+	if colourChoice == "Rainbow" {
+		messageLength := len(message)
+		for i := 0; i < messageLength; i++ {
+			r, g, b := rainbow(i)
+			fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m%s", r, g, b, message[i], reset)
 		}
+	} else {
+		var colourPicked string = colour[colourChoice]
+		fmt.Printf("%s%s%s", colourPicked, message, reset)
 	}
+}
 
-	if command == "bold" {
-		colourOfOptionPicked := Bold[colourChoice]
-		fmt.Printf("%s%s%s", colourOfOptionPicked, message, reset)
-	}
+/*
+Options: r, g, b have to be 0 or bigger and under 255
+Can error if the values are not the right size
+*/
+func PrintRGBColour(r, g, b int, message string) {
+	fmt.Printf("\033[38;2;%d;%d;%dm%s\033[0m%s", r, g, b, message, reset)
+}
 
-	if command == "underline" {
-		colourOfOptionPicked := Underline[colourChoice]
-		fmt.Printf("%s%s%s", colourOfOptionPicked, message, reset)
-	}
+/*
+Options: Black, Red, Green, Yellow, Blue, Purple, Cyan, White
+Can error if colour not found
+*/
+func PrintBold(colourChoice, message string) {
+	var colourPicked string = bold[strings.ToUpper(string(colourChoice[0]))+colourChoice[1:]]
 
-	if command == "background" {
-		colourOfOptionPicked := Background[colourChoice]
-		fmt.Printf("%s%s%s%s", colourOfOptionPicked, message, reset, reset)
-	}
+	fmt.Printf("%s%s%s", colourPicked, message, reset)
+}
 
-	if command == "high_intensity" {
-		colourOfOptionPicked := High_Intensity[colourChoice]
-		fmt.Printf("%s%s%s", colourOfOptionPicked, message, reset)
-	}
+/*
+Options: Black, Red, Green, Yellow, Blue, Purple, Cyan, White
+Can error if colour not found
+*/
+func PrintUnderline(colourChoice, message string) {
+	var colourPicked string = underline[strings.ToUpper(string(colourChoice[0]))+colourChoice[1:]]
 
-	if command == "bold_high_intensity" {
-		colourOfOptionPicked := Bold_High_Intensity[colourChoice]
-		fmt.Printf("%s%s%s", colourOfOptionPicked, message, reset)
-	}
+	fmt.Printf("%s%s%s", colourPicked, message, reset)
+}
 
-	if command == "high_intensity_backgrounds" {
-		colourOfOptionPicked := High_Intensity_backgrounds[colourChoice]
-		fmt.Printf("%s%s%s", colourOfOptionPicked, message, reset)
-	}
+/*
+Options: Black, Red, Green, Yellow, Blue, Purple, Cyan, White
+Can error if colour not found
+*/
+func PrintBackground(colourChoice, message string) {
+	var colourPicked string = background[strings.ToUpper(string(colourChoice[0]))+colourChoice[1:]]
+
+	fmt.Printf("%s%s%s%s", colourPicked, message, reset, reset)
+}
+
+/*
+Options: Black, Red, Green, Yellow, Blue, Purple, Cyan, White
+Can error if colour not found
+*/
+func PrintHighIntensity(colourChoice, message string) {
+	var colourPicked string = high_Intensity[strings.ToUpper(string(colourChoice[0]))+colourChoice[1:]]
+
+	fmt.Printf("%s%s%s", colourPicked, message, reset)
+}
+
+/*
+Options: Black, Red, Green, Yellow, Blue, Purple, Cyan, White
+Can error if colour not found
+*/
+func PrintBoldHighIntensity(colourChoice, message string) {
+	var colourPicked string = bold_High_Intensity[strings.ToUpper(string(colourChoice[0]))+colourChoice[1:]]
+
+	fmt.Printf("%s%s%s", colourPicked, message, reset)
+}
+
+/*
+Options: Black, Red, Green, Yellow, Blue, Purple, Cyan, White
+Can error if colour not found
+*/
+func PrintHighIntensityBackgrounds(colourChoice, message string) {
+	var colourPicked string = high_Intensity_backgrounds[strings.ToUpper(string(colourChoice[0]))+colourChoice[1:]]
+
+	fmt.Printf("%s%s%s", colourPicked, message, reset)
 }
