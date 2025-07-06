c3bbfe73cc61f18832219d644b49226ca3a9b78a
Jonathon Chew <>
2025-07-06 11:46:22 +0100

    fix: errors to return options, moved maps to main.go so removed from here

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package aphrodite
 
 import (
-	"errors"
 	"fmt"
 	"math"
 	"slices"
@@ -92,97 +91,21 @@ Options: Black, Red, Green, Yellow, Blue, Purple, Cyan, White
 */
 func Colour(option, color, message string) error {
 
-	colour := map[string]string{
-		"Black":  "\x1b[0;30m",
-		"Red":    "\x1b[0;31m",
-		"Green":  "\x1b[0;32m",
-		"Yellow": "\x1b[0;33m",
-		"Blue":   "\x1b[0;34m",
-		"Purple": "\x1b[0;35m",
-		"Cyan":   "\x1b[0;36m",
-		"White":  "\x1b[0;37m",
-	}
-
-	Bold := map[string]string{
-		"Black":  "\x1b[1;30m",
-		"Red":    "\x1b[1;31m",
-		"Green":  "\x1b[1;32m",
-		"Yellow": "\x1b[1;33m",
-		"Blue":   "\x1b[1;34m",
-		"Purple": "\x1b[1;35m",
-		"Cyan":   "\x1b[1;36m",
-		"White":  "\x1b[1;37m",
-	}
-
-	Underline := map[string]string{
-		"Black":  "\x1b[4;30m",
-		"Red":    "\x1b[4;31m",
-		"Green":  "\x1b[4;32m",
-		"Yellow": "\x1b[4;33m",
-		"Blue":   "\x1b[4;34m",
-		"Purple": "\x1b[4;35m",
-		"Cyan":   "\x1b[4;36m",
-		"White":  "\x1b[4;37m",
-	}
-
-	Background := map[string]string{
-		"Black":  "\x1b[40m",
-		"Red":    "\x1b[41m",
-		"Green":  "\x1b[42m",
-		"Yellow": "\x1b[43m",
-		"Blue":   "\x1b[44m",
-		"Purple": "\x1b[45m",
-		"Cyan":   "\x1b[46m",
-		"White":  "\x1b[47m",
-	}
-
-	High_Intensity := map[string]string{
-		"Black":  "\x1b[0;90m",
-		"Red":    "\x1b[0;91m",
-		"Green":  "\x1b[0;92m",
-		"Yellow": "\x1b[0;93m",
-		"Blue":   "\x1b[0;94m",
-		"Purple": "\x1b[0;95m",
-		"Cyan":   "\x1b[0;96m",
-		"White":  "\x1b[0;97m",
-	}
-
-	Bold_High_Intensity := map[string]string{
-		"Black":  "\x1b[1;90m",
-		"Red":    "\x1b[1;91m",
-		"Green":  "\x1b[1;92m",
-		"Yellow": "\x1b[1;93m",
-		"Blue":   "\x1b[1;94m",
-		"Purple": "\x1b[1;95m",
-		"Cyan":   "\x1b[1;96m",
-		"White":  "\x1b[1;97m",
-	}
-
-	High_Intensity_backgrounds := map[string]string{
-		"Black":  "\x1b[0;100m",
-		"Red":    "\x1b[0;101m",
-		"Green":  "\x1b[0;102m",
-		"Yellow": "\x1b[0;103m",
-		"Blue":   "\x1b[0;104m",
-		"Purple": "\x1b[0;105m",
-		"Cyan":   "\x1b[0;106m",
-		"White":  "\x1b[0;107m",
-	}
-
-	reset := "\x1b[0m"
-
 	acceptableOptions := []string{"Colour", "Color", "Bold", "Underline", "Background", "High_intensity", "Bold_high_intesity", "High_Intensity_backgrounds"}
 	acceptableColours := []string{"Black", "Red", "Green", "Yellow", "Blue", "Purple", "Cyan", "White", "Rainbow"}
 
 	var optionChoice string = strings.Replace(option, " ", "_", -1)
 	var colourChoice string = strings.ToUpper(string(color[0])) + color[1:]
 
+	var allAcceptableOptions string = strings.Join(acceptableOptions, ", ")
+	var allAcceptableColours string = strings.Join(acceptableColours, ", ")
+
 	if !slices.Contains(acceptableOptions, option) {
-		return errors.New(fmt.Sprint("Unable to recognise option: ", optionChoice))
+		return fmt.Errorf("unable to recognise option: %s the options are: %s", optionChoice, allAcceptableOptions)
 	}
 
 	if !slices.Contains(acceptableColours, colourChoice) {
-		return fmt.Errorf("unfortunately %s isn't a useable colour", colourChoice)
+		return fmt.Errorf("unfortunately %s isn't a useable colour the options are: %s", colourChoice, allAcceptableColours)
 	}
 
 	var command string = strings.ToLower(optionChoice)
@@ -202,37 +125,37 @@ func Colour(option, color, message string) error {
 	}
 
 	if command == "bold" {
-		colourOfOptionPicked := Bold[colourChoice]
+		colourOfOptionPicked := bold[colourChoice]
 		fmt.Printf("%s%s%s", colourOfOptionPicked, message, reset)
 		return nil
 	}
 
 	if command == "underline" {
-		colourOfOptionPicked := Underline[colourChoice]
+		colourOfOptionPicked := underline[colourChoice]
 		fmt.Printf("%s%s%s", colourOfOptionPicked, message, reset)
 		return nil
 	}
 
 	if command == "background" {
-		colourOfOptionPicked := Background[colourChoice]
+		colourOfOptionPicked := background[colourChoice]
 		fmt.Printf("%s%s%s%s", colourOfOptionPicked, message, reset, reset)
 		return nil
 	}
 
 	if command == "high_intensity" {
-		colourOfOptionPicked := High_Intensity[colourChoice]
+		colourOfOptionPicked := high_Intensity[colourChoice]
 		fmt.Printf("%s%s%s", colourOfOptionPicked, message, reset)
 		return nil
 	}
 
 	if command == "bold_high_intensity" {
-		colourOfOptionPicked := Bold_High_Intensity[colourChoice]
+		colourOfOptionPicked := bold_High_Intensity[colourChoice]
 		fmt.Printf("%s%s%s", colourOfOptionPicked, message, reset)
 		return nil
 	}
 
 	if command == "high_intensity_backgrounds" {
-		colourOfOptionPicked := High_Intensity_backgrounds[colourChoice]
+		colourOfOptionPicked := high_Intensity_backgrounds[colourChoice]
 		fmt.Printf("%s%s%s", colourOfOptionPicked, message, reset)
 		return nil
 	}
